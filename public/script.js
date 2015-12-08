7eb0a31cc89f0cb000cb71f68cd0727c6d3b74e2
anae75 <ana.devel@gmail.com>
2015-12-08 17:56:12 -0500

    2 way data binding between input and div

--- public/script.js
+++ public/script.js
@@ -6,20 +6,37 @@ class App extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = { timers: [5, 99, 42, 10] };
+    this.state = { 
+      timers: [5, 99, 42, 10],
+      name:  'Dragons!'
+      };
   }
-  content() {
+  timerContent() {
     return this.state.timers.map((seconds, i) =>
       <Timer startingSeconds={seconds} key={i} />
     );
   }
+  changeInput(e) {
+    //console.log(e.target.value);
+    this.setState({name: e.target.value});
+  }
   render() {
     return (
       <div>
-        {this.content()}
+        <div>
+          {this.timerContent()}
+        </div>
+        <div>
+         <input value={this.state.name} onChange={this.changeInput.bind(this)} />
+         <div>
+           {this.state.name}
+         </div>
+        </div>
       </div>
+
     );
   }
 }
 
+// To see this in the console, click on the React tab of the chrome developer tools
 ReactDOM.render(<App />, document.getElementById('react'));
